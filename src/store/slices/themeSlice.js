2aee7f630a5366e7f7a5684b17c663a5ff04e676
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2025-12-11 12:11:50 +0600

    :package: set up redux

---
+++ src/store/slices/themeSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  platformName: 'NextGen Pros',
+  tagline: 'Next-generation digital platform for youth football talent discovery',
+  colors: {
+    primaryCyan: '#00E5FF',
+    primaryMagenta: '#9C27B0',
+    backgroundDark: '#0B0D2C',
+    backgroundCard: '#12143A',
+    neonAccent: '#14F1D9'
+  }
+}
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    updatePlatformName: (state, action) => {
+      state.platformName = action.payload
+    },
+    updateTagline: (state, action) => {
+      state.tagline = action.payload
+    },
+    updateColor: (state, action) => {
+      const { colorKey, value } = action.payload
+      state.colors[colorKey] = value
+    },
+    resetTheme: () => initialState
+  }
+})
+
+export const { updatePlatformName, updateTagline, updateColor, resetTheme } = themeSlice.actions
+export default themeSlice.reducer
+
+// src/store/store.js
+import { configureStore } from '@reduxjs/toolkit'
+import themeReducer from './slices/themeSlice'
+
+export const store = configureStore({
+  reducer: {
+    theme: themeReducer
+  }
+})
