f69ba14e2ac140a4eb73835ac6de4402a837088f
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-02-25 11:51:30 +0600

    this is test commit

--- src/app/(dashboard)/player/events/[id]/register/page.jsx
+++
@@ -1,6 +0,0 @@
-
-import EventRegistrationPage from "@/components/player/event/EventRegistrationPage";
-
-export default function Page({ params }) {
-  return <EventRegistrationPage params={params} />;
-}
--- src/app/(dashboard)/scout/events/[id]/register/page.jsx
+++
@@ -1,5 +0,0 @@
-import ScoutEventRegistrationPage from "@/components/scout/event/ScoutEventRegistrationPage";
-
-export default function Page({ params }) {
-  return <ScoutEventRegistrationPage params={params} />;
-}
