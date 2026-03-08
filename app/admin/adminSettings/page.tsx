fac7a5032965ee03a80344ae44b20fa5e83929fb
Abrrar <madborakash48@gmail.com>
2026-03-08 14:03:33 +0600

    this commit for the login fixed

--- app/admin/adminSettings/page.tsx
+++ app/admin/adminSettings/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AdminSettingsPage = () => {
   return (
     <div>
-      
+      <h2 className='text-6xl text-white'> admin settings admin</h2>
     </div>
   )
 }
--- app/admin/userManagement/page.tsx
+++ app/admin/userManagement/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UserManagementPage = () => {
   return (
     <div>
-      
+      <h2 className='text-6xl text-white'> user management admin</h2>
     </div>
   )
 }
